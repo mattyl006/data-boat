272d855cedae822aae7b2bb732036a4f9ee496b3
Mateusz Tylka <mattyl@st.amu.edu.pl>
2023-10-13 13:53:00 +0200

    synchro two pages test

--- src/pages/StartPage.js
+++ src/pages/StartPage.js
@@ -4,10 +4,33 @@ import { H1 } from '../utils/fonts';
 import { Link } from 'react-router-dom';
 
 const StartPage = () => {
+  const [name, setName] = React.useState('');
+
+  const onStorageUpdate = (e) => {
+    const { key, newValue } = e;
+    if (key === 'name') {
+      setName(newValue);
+    }
+  };
+
+  const handleChange = (e) => {
+    setName(e.target.value);
+    localStorage.setItem('name', e.target.value);
+  };
+
+  React.useEffect(() => {
+    setName(localStorage.getItem('name') || '');
+    window.addEventListener('storage', onStorageUpdate);
+    return () => {
+      window.removeEventListener('storage', onStorageUpdate);
+    };
+  }, []);
+
   return (
     <FlexColumn width="100%" height="calc(100vh - 48px)">
       <H1>StartPage</H1>
       <Link to="/next-page">NextPage</Link>
+      <input value={name} onChange={handleChange} />
     </FlexColumn>
   );
 };
